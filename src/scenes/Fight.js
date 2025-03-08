a763e14680220bfde6fee78eac7be382ccf40544
Ananya Setty <asetty1@ucsc.edu>
2025-03-07 19:33:13 -0800

    working on button mashing, implemented randomm need to get tweaks through

--- src/scenes/Fight.js
+++ src/scenes/Fight.js
@@ -1,20 +1,48 @@
 class Fight extends Phaser.Scene {
     constructor() {
-        super("fightscene")
+        super("fightscene");
     }
 
     preload() {
-        this.load.path = "./assets/"
+        this.load.path = "./assets/";
 
         // map assets
-        this.load.image('bg', 'fightbg.png')
-        this.load.image('windmill', 'windmill.png')
+        this.load.image('bg', 'fightbg.png');
+        this.load.image('windmill', 'windmill.png');
+
+        this.load.spritesheet('xmash', 'xmash.png', {
+            frameWidth: 100,
+            frameHeight: 80,
+            startFrame: 0,
+            endFrame: 1
+        });
+        
+        this.load.spritesheet('amash', 'amash.png', {
+            frameWidth: 100,
+            frameHeight: 80,
+            startFrame: 0,
+            endFrame: 1
+        });
+
+        this.load.spritesheet('tmash', 'tmash.png', {
+            frameWidth: 100,
+            frameHeight: 80,
+            startFrame: 0,
+            endFrame: 1
+        });
+
+        this.load.spritesheet('hmash', 'hmash.png', {
+            frameWidth: 100,
+            frameHeight: 80,
+            startFrame: 0,
+            endFrame: 1
+        });
     }
 
     create() {
-        this.add.image(0, 0, 'bg').setOrigin(0)
+        this.add.image(0, 0, 'bg').setOrigin(0);
 
-        let windmill = this.add.image(-150, 220, 'windmill')
+        let windmill = this.add.image(-150, 220, 'windmill');
         let windmillTween = this.tweens.add({
             delay: 125,
             targets: windmill,
@@ -23,31 +51,135 @@ class Fight extends Phaser.Scene {
             duration: 250,
             repeat: 0,
             paused: false
-        })
+        });
 
-        windmillTween.play()
+        windmillTween.play();
 
-        //button  mashing! (based of of yakuza)
-        this.mashCount = 0
-        this.target = 20
-        this.mashTimer = 3000
+        // Create animations
+        this.anims.create({
+            key: 'xmash',
+            frames: this.anims.generateFrameNumbers('xmash', {
+                start: 0,
+                end: 1
+            }),
+            frameRate: 7,
+            repeat: -1
+        });
 
-        this.mashKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A)
+        this.anims.create({
+            key: 'amash',
+            frames: this.anims.generateFrameNumbers('amash', {
+                start: 0,
+                end: 1
+            }),
+            frameRate: 7,
+            repeat: -1
+        });
 
-        this.mashKey.on('down', () => {
-            this.mashCount++;
-        })
-        console.log("Press the A key!")
-        this.add.text(400, 100, 'Mash the A key!', {font: '30px damascus'})
-        this.time.delayedCall(this.mashTimer, this.checkMashCount, [], this)
+        this.anims.create({
+            key: 'tmash',
+            frames: this.anims.generateFrameNumbers('tmash', {
+                start: 0,
+                end: 1
+            }),
+            frameRate: 7,
+            repeat: -1
+        });
 
+        this.anims.create({
+            key: 'hmash',
+            frames: this.anims.generateFrameNumbers('hmash', {
+                start: 0,
+                end: 1
+            }),
+            frameRate: 7,
+            repeat: -1
+        });
+
+        this.mashCount = 0;
+        this.target = 2;
+        this.mashTimer = 3000;
+
+        this.keys = [
+            Phaser.Input.Keyboard.KeyCodes.X,
+            Phaser.Input.Keyboard.KeyCodes.A,
+            Phaser.Input.Keyboard.KeyCodes.H,
+            Phaser.Input.Keyboard.KeyCodes.T
+        ];
+
+        this.currentKey = null;
+        this.mashSprite = null;
+
+        this.startButtonMashing();
+    }
+
+    startButtonMashing() {
+        this.time.delayedCall(1000, this.pickRandomFunction, [], this);
+    }
+
+    pickRandomFunction() {
+        let functions = [this.functionX, this.functionA, this.functionH, this.functionT];
+        let randomIndex = Phaser.Math.Between(0, functions.length - 1);
+        functions[randomIndex].call(this);
+    }
+
+    functionX() {
+        this.handleMash('X', 'xmash');
+    }
+
+    functionA() {
+        this.handleMash('A', 'amash');
+    }
+
+    functionH() {
+        this.handleMash('H', 'hmash');
+    }
+
+    functionT() {
+        this.handleMash('T', 'tmash');
+    }
+
+    handleMash(key, anim) {
+        this.currentKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes[key]);
+        this.mashCount = 0;
+        console.log(`Start mashing the ${key} key!`);
+
+        if (this.mashSprite) {
+            this.mashSprite.destroy();
+        }
+
+        this.mashSprite = this.add.sprite(400, 300, anim);
+        this.mashSprite.play(anim);
+
+        this.input.keyboard.on('keydown-' + key, this.countMash, this);
+        this.time.delayedCall(this.mashTimer, this.checkMashCount, [], this);
+    }
+
+    countMash() {
+        this.mashCount++;
     }
 
     checkMashCount() {
-        if (this.mashCount >= this.target) {
-            console.log("Yay yuou did it!!")
-        } else {
-            console.log("You Lost :(((")
+        if (this.currentKey) {
+            this.input.keyboard.off('keydown-' + this.currentKey.keyCode, this.countMash, this);
         }
+
+        if (this.mashCount >= this.target) {
+            console.log("Yay, you did it!!");
+        } else {
+            console.log("You Lost :(((");
+        }
+
+        this.time.delayedCall(5000, this.pickRandomFunction, [], this);
+    }
+
+    getKeyCharacter(keyCode) {
+        const keyMap = {
+            88: 'X', // Key code for 'X'
+            65: 'A', // Key code for 'A'
+            72: 'H', // Key code for 'H'
+            84: 'T'  // Key code for 'T'
+        };
+        return keyMap[keyCode] || 'Unknown Key';
     }
 }
